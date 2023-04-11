70657dc2b99dc666bcfb9ea0ade40e52c096cf9c
Sérgio Lima <sergiolimafg3@gmail.com>
2023-04-11 18:23:20 -0300

    implementado  <BrowserRouter>       <Routes>         <Route path="/" element = {<Inicio/>}/>         <Route path="/SobreMim" element = {<SobreMim/>}/>         <Route path="*" element = {<div>Página não encontrada!</div>}/>       </Routes>     </BrowserRouter>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from "./paginas/Inicio";
+import SobreMim from './paginas/SobreMim/index';
 
 
 
 function App() {
   return (
     <BrowserRouter>
-      
+      <Routes>
+        <Route path="/" element = {<Inicio/>}/>
+        <Route path="/SobreMim" element = {<SobreMim/>}/>
+        <Route path="*" element = {<div>Página não encontrada!</div>}/>
+      </Routes>
     </BrowserRouter>
   ); 
 }
