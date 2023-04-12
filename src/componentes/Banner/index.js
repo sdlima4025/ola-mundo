e3a2688088b355b78247187b7458b90d423d3acc
Sérgio Lima <sergiolimafg3@gmail.com>
2023-04-12 11:13:50 -0300

    criado banner com suas props

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,5 +1,35 @@
 import styles from './Banner.module.css'
-
+import circuloColorido from 'assets/circulo_colorido.png'
+import minhaFoto from 'assets/fotoPerfil.png'
 export default function Banner() {
-  return "Banner"
+  return (
+    <div className={styles.banner}>
+        <div className={styles.apresentacao}>
+          <h1 className={styles.titulo}>
+            Olá Mundo!
+          </h1>
+
+          <p className={styles.paragrafo}>
+          Boas vindas ao meu espaço pessoal! Eu sou Sérgio Lima, 
+          aluno Front-end da Alura. 
+          Aqui compartilho vários conhecimentos, espero que aprenda algo novo juntos :)
+          </p>
+        </div>
+
+        <div className={styles.imagens}>
+          <img 
+              className={styles.circuloColorido}
+              src={circuloColorido} 
+              aria-hidden={true}
+              alt='Circulo colorido'
+          />
+
+          <img 
+            className={styles.minhaFoto}
+            src={minhaFoto}
+            alt='Foto do Sérgio Lima Sorrindo'
+          />
+        </div>
+    </div>
+  )
 }
