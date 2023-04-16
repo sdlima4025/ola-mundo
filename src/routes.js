6375b60947ea782425cb282ffcc81fdacaf8d0b8
Sérgio Lima <sergiolimafg3@gmail.com>
2023-04-15 22:54:37 -0300

    ajustes em Routes com fefatoração de PostModelo,

--- src/routes.js
+++ src/routes.js
@@ -20,10 +20,9 @@ function AppRoutes() {
         <Route path="/" element={<PaginaPadrao/>}>
         <Route index element = {<Inicio/>}/>
         <Route path="SobreMim" element = {<SobreMim/>}/>
-        <Route path="posts/:id" element={<Post/>} />
       </Route>
-
-
+      
+        <Route path="posts/:id" element={<Post/>} />
         <Route path="*" element = {<NaoEncontrada/>}/>
       </Routes>
 
